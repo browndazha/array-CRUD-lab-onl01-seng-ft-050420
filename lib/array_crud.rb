9c32bc15a51172a4eea784abeaae2595a9485d14
Dazha Brown <dajahbrown@hotmail.com>
2020-05-11 22:54:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,11 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  designers = ["YSL","Dior","LV","Chanel"]
-   designers[0]
-   designers[1]
-   designers[2]
-   designers[3]
+   array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
